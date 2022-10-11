8a9c9f408ffa240e5e89a47c691c3ce843777bf8
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-10-11 18:27:46 +0500

    Onboarding Screen Done

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:go_jobs/components.dart';
+
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
 
@@ -8,12 +10,48 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          // Image
+          Expanded(
+            child: Image.asset(
+              Theme.of(context).brightness == Brightness.light
+                  ? 'assets/images/onbording_image_light.png'
+                  : 'assets/images/onbording_image_dark.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: SafeArea(
+              top: false,
+              child: Column(
+                children: [
+                  Spacer(),
+                  // Title
+                  Text(
+                    'Looking for a dream job?',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: kDefaultPadding / 2),
+                  // Text
+                  Text(
+                    'Now you can easily find you dream job\nHappy Searching!',
+                    textAlign: TextAlign.center,
+                  ),
+                  Spacer(flex: 2),
 
-          // Title
-          // Text
-
-          // Button
+                  // Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Get Started'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
