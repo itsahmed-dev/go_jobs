e34ebfbe1d52c55776beae05a975916cabe0f520
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-10-11 18:33:05 +0500

    Onboarding Screen Done

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
+import 'package:go_jobs/components.dart';
+
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
     return Scaffold(
       body: Column(
         children: [
-          // Image
+          Expanded(
+            child: Image.asset(
+              themeData.brightness == Brightness.light
+                  ? 'assets/images/onbording_image_light.png'
+                  : 'assets/images/onbording_image_dark.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: SafeArea(
+              top: false,
+              child: Column(
+                children: [
+                  Spacer(),
+                  // Title
+                  Text(
+                    'Looking for a dream job?',
+                    style: themeData.textTheme.titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: kDefaultPadding / 2),
+                  // Text
+                  Text(
+                    'Now you can easily find you dream job\nHappy Searching!',
+                    textAlign: TextAlign.center,
+                  ),
+                  Spacer(flex: 2),
 
-          // Title
-          // Text
-
-          // Button
+                  // Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Get Started'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/themes.dart
+++ lib/themes.dart
@@ -6,10 +6,33 @@ final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: kPrimaryColor,
   textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: kPrimaryColor,
+      padding: EdgeInsets.all(kDefaultPadding),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(kDefaultBorderRadius),
+        ),
+      ),
+    ),
+  ),
 );
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: kPrimaryColor,
+  scaffoldBackgroundColor: kBackgroundColorDarkTheme,
   textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: kPrimaryColor,
+      padding: EdgeInsets.all(kDefaultPadding),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(kDefaultBorderRadius),
+        ),
+      ),
+    ),
+  ),
 );
